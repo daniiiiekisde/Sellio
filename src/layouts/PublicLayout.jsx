50e15e0eef96e17be904b6470cb5b92d4060dee8
Daniil Tikhomirov <87585663+daniiiiekisde@users.noreply.github.com>
2026-08-19 18:32:14 +0200

    feat: activate public pages experience

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { Navbar, Footer } from '../components/navigation';
 import './PublicLayout.css';
 import './PublicExperience.css';
+import './PublicPagesExperience.css';
 
 export const PublicLayout = () => {
   return (
